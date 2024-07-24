43f96009b027d889eec62f30e2bb331b7c3f06d6
Raghav Pandit <panditraghav18@gmail.com>
2024-07-24 21:44:46 +0530

    Added chamchunk font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 import "./page.css";
 import localFont from "next/font/local";
 
-const poppins = localFont({
-  src: "./Poppins-Regular.ttf",
-  display: "swap",
-});
 const chamchunk = localFont({
-  src: "./chamchunk-400.otf",
+  src: "../fonts/chamchunk-400.otf",
   display: "swap",
 });
 
 export default function Home() {
   return (
     <main>
-      <h1 className={poppins.className}>Poppins</h1>
       <h1
         className={chamchunk.className}
         style={{ fontSize: 96, letterSpacing: 4 }}
