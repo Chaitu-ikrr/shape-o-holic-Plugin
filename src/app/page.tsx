a967d0b25379621ed8272c797d0ac5528fc17ff9
Raghav Pandit <panditraghav18@gmail.com>
2024-08-02 15:03:28 +0530

    Removed chamchunk font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import localFont from "next/font/local";
 import style from "./page.module.css";
 import ShapeOHolicSvg from "@/components/ShapeOHolicSvg";
-
-const chamchunk = localFont({
-  src: "../fonts/chamchunk-400.otf",
-  display: "swap",
-});
 
 export default function Home() {
   return (
